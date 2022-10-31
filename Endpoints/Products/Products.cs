029c327715cccf9fefce6865531f80aee8686f92
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-10-31 13:53:47 -0400

    feat: added methods post put and delete

--- Endpoints/Products/Products.cs
+++ Endpoints/Products/Products.cs
@@ -47,5 +47,75 @@ public static class Products
             }
         }).WithTags("Products")
             .Produces<Product>();
+        
+        app.MapGet("/products/{id:Guid}", (AppDbContext db, Guid id) =>
+        {
+            try
+            {
+                if (db.Products is null)
+                    return Results.NotFound();
+
+                var product = db.Products.FirstOrDefault(p => p.ProductId == id);
+                if (product is null)
+                    return Results.NotFound();
+
+                return Results.Ok(product);
+            }
+            catch (Exception e)
+            {
+                return Results.BadRequest(e.Message);
+            }
+        }).WithTags("Products")
+            .Produces<Product>();
+        
+        app.MapPut("/products/{id:Guid}", async (AppDbContext db, Guid id, ProductRequest productRequest) =>
+        {
+            try
+            {
+                if (db.Products is null)
+                    return Results.NotFound();
+
+                var product = db.Products.FirstOrDefault(p => p.ProductId == id);
+                if (product is null)
+                    return Results.NotFound();
+
+                product.Name = productRequest.Name;
+                product.Description = productRequest.Description;
+                product.Price = productRequest.Price;
+                product.Image = productRequest.Image;
+                product.CategoryId = productRequest.categoryId;
+                
+                db.Products.Update(product);
+                await db.SaveChangesAsync();
+                return Results.Ok(product);
+            }
+            catch (Exception e)
+            {
+                return Results.BadRequest(e.Message);
+            }
+        }).WithTags("Products")
+            .Produces<Product>();
+        
+        app.MapDelete("/products/{id:Guid}", async (AppDbContext db, Guid id) =>
+        {
+            try
+            {
+                if (db.Products is null)
+                    return Results.NotFound();
+
+                var product = db.Products.FirstOrDefault(p => p.ProductId == id);
+                if (product is null)
+                    return Results.NotFound();
+
+                db.Products.Remove(product);
+                await db.SaveChangesAsync();
+                return Results.Ok(product);
+            }
+            catch (Exception e)
+            {
+                return Results.BadRequest(e.Message);
+            }
+        }).WithTags("Products")
+            .Produces<Product>();
     }
 }
